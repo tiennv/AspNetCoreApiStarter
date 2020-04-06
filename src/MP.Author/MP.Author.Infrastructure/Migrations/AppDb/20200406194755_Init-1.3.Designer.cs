5c0058ed3a7608c9bd391801d7250a4f3ee9b936
TIENNV\tiennv <tiennv.hp@gmail.com>
2020-04-07 02:48:21 +0700

    Update EF

--- src/MP.Author/MP.Author.Infrastructure/Migrations/AppDb/20200406194755_Init-1.3.Designer.cs
+++ src/MP.Author/MP.Author.Infrastructure/Migrations/AppDb/20200406194755_Init-1.3.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MP.Author.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MP.Author.Infrastructure.Migrations.AppDb
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200406194755_Init-1.3")]
+    partial class Init13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MP.Author/MP.Author.Infrastructure/Migrations/AppDb/20200406194755_Init-1.3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MP.Author.Infrastructure.Migrations.AppDb
+{
+    public partial class Init13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Menus",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Menus");
+        }
+    }
+}
